7bc12e8a29a7b1c154142f8720a64dc6b368be3f
Alexandre Magno <alexandre_mcti@hotmail.com>
2025-11-21 19:03:14 -0300

    refactor: atualiza pedid

--- qualificacao-ms/src/application/services/qualification.ts
+++ qualificacao-ms/src/application/services/qualification.ts
@@ -1,6 +1,7 @@
 import STATE from "../../main/shared/data/state";
 import { QualificacaoServicePort, QualificacaoServicePortInputDTO } from "@ports/in/qualificacao";
 import { QualificacaoOutput } from "../../ports/out/qualificacao";
+import logger from "@shared/logs";
 
 
 export class QualificacaoService implements QualificacaoServicePort {
@@ -12,9 +13,11 @@ export class QualificacaoService implements QualificacaoServicePort {
         if(!isOrderQualified) {
             const order = {...input, status: 'recusado'}
             await this.qualificacaoOutput.OrderRecused(order)
+            logger.info(`Order ${input.id} refused. State ${input.customer.deliveryAddress.state} is not qualified.`)
             return
         }
         const order = {...input, status: 'qualificado'}
         await this.qualificacaoOutput.OrderQualified(order)
+        logger.info(`Order ${input.id} qualified successfully.`)
     }
 }
